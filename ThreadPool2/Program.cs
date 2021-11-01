4e646c734b8e87989570bf353a1bd13cdaf37e88
CmaillaKurup <cami670v@zbc.dk>
2021-11-01 15:57:21 +0100

    test the speed of both a Thread and Thread pool

--- ThreadPool2/Program.cs
+++ ThreadPool2/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace ThreadPool2
 {
     class Program
     {
+        //This code test the speed of both a Thread and Thread pool
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stopwatch mywatch = new Stopwatch();
+            Console.WriteLine("Threat pool are executing here");
+            
+            mywatch.Start();
+            
+            ProcessWithThreadPoolMethod();
+            mywatch.Stop();
+            
+            Console.WriteLine("Time consumed by ProcessWithThreadPoolMethod is : " + mywatch.ElapsedTicks);
+            
+            mywatch.Reset();
+            
+            Console.WriteLine("threats are executing here");
+            
+            mywatch.Start();
+            ProcessWithThreadMethod();
+            mywatch.Stop();
+            
+            Console.WriteLine("Time consumed by ProcessWithThreadMethod is : " + mywatch.ElapsedTicks);
+            
+        }
+
+        static void Process(object obj)
+        {
+            
+        }
+        
+        static void ProcessWithThreadMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread obj = new Thread(Process);
+                obj.Start();
+            }
+        }
+        static void ProcessWithThreadPoolMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Process);
+            }
         }
     }
 }
